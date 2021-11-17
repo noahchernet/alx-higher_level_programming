b2b3091f2c0aec4b9f18c950b5edd29ed2b912a0
Noah Chernet <noahchernet@yahoo.com>
2021-11-17 20:25:23 +0300

    Changed print arguments to string format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -7,8 +7,8 @@ def uppercase(str):
         if 97 <= ord(str[i]) <= 122:
             if i == len(str) - 1:
                 n = '\n'
-            print(chr(ord(str[i]) - 32) + n, end="")
+            print("{}{}".format(chr(ord(str[i]) - 32), n), end="")
         else:
             if i == len(str) - 1:
                 n = '\n'
-            print(str[i] + n, end="")
+            print("{}{}".format(str[i], n), end="")
