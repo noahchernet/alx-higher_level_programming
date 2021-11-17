ca790969b58be47d1c733d9f174fb38975471ee5
Noah Chernet <noahchernet@yahoo.com>
2021-11-17 16:44:38 +0300

    Updated 1-last_digit.py to return negative numbers correctly

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10 if number > 0 else -1 * (number % 10)
+last_digit = number % 10 if number > 0 else -1 * (abs(number) % 10)
 print("Last digit of", number, "is", last_digit, "and is ", end="")
 
 if last_digit > 5:
