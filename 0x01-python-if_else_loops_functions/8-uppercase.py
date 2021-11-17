1305c31b9a14c496cdc24e2b3b92a4dd62b0bb0a
Noah Chernet <noahchernet@yahoo.com>
2021-11-17 22:36:41 +0300

    Handled NULL str for newly designed code

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,10 @@
 
 
 def uppercase(str):
+    if str == "":
+        print()
+        return
+
     for i in range(0, len(str)):
         n = '\n' if i == len(str) - 1 else ''
         print("{}{}".format(chr(ord(str[i]) - 32), n)
