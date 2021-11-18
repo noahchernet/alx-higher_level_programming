a0dd2c68b34d5e84fbf19d8a7346d86f2c807c70
Noah Chernet <noahchernet@yahoo.com>
2021-11-18 19:50:42 +0300

    Done task 2, 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
+from sys import argv
 
 
+def main():
+    if len(argv) - 1 == 0:
+        print("0 arguments.")
+        return
+    elif len(argv) - 1 == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+    for i in range(len(argv) - 1):
+        print("{}: {}".format(i + 1, argv[i + 1]))
+
+
+if __name__ == '__main__':
+    main()
