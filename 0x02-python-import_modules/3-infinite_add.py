8cd65641496827adeba3eba2a291f517515c78a8
Noah Chernet <noahchernet@yahoo.com>
2021-11-18 20:13:10 +0300

    Done task 3 & 4, 3-infinite_add.py and 4-hidden_discovery.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+from sys import argv
 
 
+def main():
+    final_sum = 0
+
+    for i in range(len(argv) - 1):
+        final_sum += int(argv[i + 1])
+    print(final_sum)
+
+
+if __name__ == '__main__':
+    main()
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+import hidden_4
 
 
+def main():
+    for i in dir(hidden):
+        if i[:2] != '__':
+            print(i)
+
+
+if __name__ == "__main__":
+    main()
