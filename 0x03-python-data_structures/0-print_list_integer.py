2538acb6898a871191ed25255dad6c4ba1a56500
Noah Chernet <noahchernet@yahoo.com>
2021-11-20 11:32:55 +0300

    Done task 0, 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 
-
+def print_list_integer(my_list=[]):
+    if len(my_list) == 0:
+        print()
+        return
+    str.format('2')
+    for i in my_list:
+        print("{}".format(i))
