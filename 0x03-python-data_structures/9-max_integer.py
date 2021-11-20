2931beb6f6b0bc35880c4804a1c4ac58ea4729b4
Noah Chernet <noahchernet@yahoo.com>
2021-11-20 11:38:23 +0300

    Done task 9, 9-max_integer.py and modified 9-main.py to add more tests

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 
 
+def max_integer(my_list=[]):
+    if my_list is None or len(my_list) == 0:
+        return None
+    max_num = 0
+    for i in my_list:
+        if max_num < i:
+            max_num = i
+
+    return max_num
