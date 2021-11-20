822c1b1561ddf7147aa52f02bd511d6e7fb28ea1
Noah Chernet <noahchernet@yahoo.com>
2021-11-20 11:38:37 +0300

    Done task 10, 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 
 
+def divisible_by_2(my_list=[]):
+    # Holds boolean values that correspond to each value in my_list
+    mul_of_2 = []
+
+    for i in my_list:
+        mul_of_2.append(True) if i % 2 == 0 else mul_of_2.append(False)
+
+    return mul_of_2
