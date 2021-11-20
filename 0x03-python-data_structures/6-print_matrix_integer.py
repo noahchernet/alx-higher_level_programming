37aeb80bf325879aa293420fad46806d107350e2
Noah Chernet <noahchernet@yahoo.com>
2021-11-20 11:37:35 +0300

    Done task 6, 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 
 
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for j in i:
+            print("{}".format((str(j) + " ") if j != i[-1] else j), end="")
+        print()
