1d5bdcaf7b48392e04485424171aaea9218398d5
Noah Chernet <noahchernet@yahoo.com>
2021-11-20 12:25:29 +0300

    Done task 13, 13-is_palindrome.c. Modified 13-main.c to add more tests. Betty style warnings fixed for linked_lists.c and lists.h. Added function declaration in lists.h

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,1 +1,52 @@
 #include "lists.h"
+
+/**
+ * is_palindrome - checks if linked list @head is a palindrome
+ * @head: the linked list to be checked if it's a palindrome
+ * Return: 1 if it's a palindrome, 0 otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+	int i, list_len = (int) listint_len(*head);
+	int *h = malloc(sizeof(int) * list_len); /* Contains all n's in *head*/
+	listint_t *head_copy = *head;
+
+	if (!*head || ((*head)->n == 0 && (*head)->next == NULL))
+		return (1);
+
+	for (i = 0; i < list_len; i++)
+	{
+		h[i] = (*head)->n;
+		*head = (*head)->next;
+	}
+	*head = head_copy; /* Return the head of the list back to the first item */
+
+	for (i = 0; i < list_len; i++)
+	{
+		if (h[i] == h[list_len - 1 - i])
+			continue;
+		free(h);
+		return (0);
+	}
+
+	free(h);
+	return (1);
+}
+
+/**
+ * listint_len - length of a list
+ * @h:pointer to list element
+ * Return: size of list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+
+	return (len);
+}
--- 0x03-python-data_structures/13-main.c
+++ 0x03-python-data_structures/13-main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
@@ -9,27 +8,38 @@
  */
 int main(void)
 {
-    listint_t *head;
+	listint_t *head;
 
-    head = NULL;
-    add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 17);
-    add_nodeint_end(&head, 972);
-    add_nodeint_end(&head, 50);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 50);
-    add_nodeint_end(&head, 972);
-    add_nodeint_end(&head, 17);
-    add_nodeint_end(&head, 1);
-    print_listint(head);
+	head = NULL;
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n-- -- --\n");
+	else
+		printf("Linked list is not a palindrome\n-- -- --\n");
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 17);
+	add_nodeint_end(&head, 972);
+	add_nodeint_end(&head, 50);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 50);
+	add_nodeint_end(&head, 972);
+	add_nodeint_end(&head, 17);
+	add_nodeint_end(&head, 1);
+	print_listint(head);
 
-    if (is_palindrome(&head) == 1)
-        printf("Linked list is a palindrome\n");
-    else
-        printf("Linked list is not a palindrome\n");
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
 
-    free_listint(head);
+	free_listint(head);
+	printf("\n-- -- --\n");
 
-    return (0);
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+
+
+	return (0);
 }
