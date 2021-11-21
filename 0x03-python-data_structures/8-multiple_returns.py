c85896ac303754cb40ed27be815045bb3489e7d0
Noah Chernet <noahchernet@yahoo.com>
2021-11-21 07:19:55 +0300

    Updated 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 
 
 def multiple_returns(sentence):
-    if sentence is "" or None:
+    if sentence is "" or sentence is None:
         return tuple((0, None))
     return tuple((len(sentence), sentence[0]))
