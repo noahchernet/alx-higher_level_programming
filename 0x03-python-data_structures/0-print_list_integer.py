4e904ff688e93dbeda374e5a92a95af49c47b1f8
Noah Chernet <noahchernet@yahoo.com>
2021-11-21 06:59:28 +0300

    0-print_list_integer.py now uses str.format() when printing

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
+
 def print_list_integer(my_list=[]):
     if len(my_list) == 0:
         print()
         return
     str.format('2')
     for i in my_list:
-        print("{}".format(i))
+        print(str.format(i))
