132429631bb575a039aa2a31b2495cf34fc5a494
Noah Chernet <noahchernet@yahoo.com>
2021-11-21 07:02:03 +0300

    Updated 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -5,6 +5,5 @@ def print_list_integer(my_list=[]):
     if len(my_list) == 0:
         print()
         return
-    str.format('2')
     for i in my_list:
-        print(str.format(i))
+        print(str.format(str(i)))
