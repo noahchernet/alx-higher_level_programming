163fd9ebb08fc080cf4d6a4f70536fdd11c396b0
Noah Chernet <noahchernet@yahoo.com>
2021-11-21 08:24:19 +0300

    Modified string formatting to display integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            print("{}".format((str(j) + " ") if j != i[-1] else j), end="")
+            print("{:d}".format((str(j) + " ") if j != i[-1] else j), end="")
         print()
