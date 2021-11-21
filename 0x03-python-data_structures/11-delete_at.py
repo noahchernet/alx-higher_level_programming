56b4f0d7eb0f16da91baca7afab0470f87a9cd29
Noah Chernet <noahchernet@yahoo.com>
2021-11-21 08:49:08 +0300

    11-delete_at.py now uses del keyword to delete element

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -6,11 +6,6 @@ def delete_at(my_list=[], idx=0):
         return None
     if idx < 0 or idx >= len(my_list):
         return my_list
-    new_list = []
 
-    for i in range(len(my_list)):
-        if i != idx:
-            new_list.append(my_list[i])
-
-    my_list = new_list
-    return new_list
+    del my_list[idx]
+    return my_list
