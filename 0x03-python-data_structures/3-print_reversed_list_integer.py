e941ae29412aecb633d3acca1b6c630ff52e07d9
Noah Chernet <noahchernet@yahoo.com>
2021-11-21 08:23:03 +0300

    Modified string formatting to display integers

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,5 +4,5 @@
 def print_reversed_list_integer(my_list=[]):
     i = -1
     while i >= -1 * len(my_list):
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
         i -= 1
