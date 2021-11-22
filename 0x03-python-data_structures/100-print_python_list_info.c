e0a9d697f99e051090c6d061b94356ec1f86c343
Noah Chernet <noahchernet@yahoo.com>
2021-11-22 20:30:28 +0300

    Added Py<Type>_Check functions to figure out *p's type

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -16,7 +16,16 @@ void print_python_list_info(PyObject *p)
 
 	for(i = 0; i < (int) PyList_Size(p); i++)
 	{
-		printf("Element %d: %s\n", i, (char *) PyObject_Str(PyList_GetItem(p,
-																		 i)));
+		if (PyLong_Check(p))
+			printf("Element %d: int\n", i);
+		else if (PyFloat_Check(p))
+			printf("Element %d: float\n", i);
+		else if (PyString_Check(p))
+			printf("Element %d: str\n", i);
+		else if (PyList_Check(p))
+			printf("Element %d: list\n", i);
+		else if (PyTuple_Check(p))
+			printf("Element %d: tuple\n", i);
+		else if ()
 	}
 }
