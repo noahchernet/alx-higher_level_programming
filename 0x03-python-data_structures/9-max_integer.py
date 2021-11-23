ab668078d0ea5e3bf706810b3fe297c2ed94e1e5
Noah Chernet <noahchernet@yahoo.com>
2021-11-23 09:52:03 +0300

    Changed initial val of max_num to handle negative numbers in my_list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@
 def max_integer(my_list=[]):
     if my_list is None or my_list == []:
         return None
-    max_num = 0
+    max_num = my_list[0]
     for i in my_list:
         if max_num < i:
             max_num = i
