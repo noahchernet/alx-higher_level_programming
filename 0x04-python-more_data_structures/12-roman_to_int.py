710513172365281ee325c9fb03b3d13986ae9ef1
Noah Chernet <noahchernet@yahoo.com>
2021-11-24 21:46:27 +0300

    Return of None case modified to 0

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@
 
 def roman_to_int(roman_string):
     if roman_string != str(roman_string) or roman_string is None:
-        return None
+        return 0
     symbols = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     nums = []
 
