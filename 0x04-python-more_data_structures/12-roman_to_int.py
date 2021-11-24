653c50cc2ef0adaf2b8ef6ca68fee21d7d75131a
Noah Chernet <noahchernet@yahoo.com>
2021-11-24 21:53:34 +0300

    12-roman_to_int.py simplified

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,15 +5,15 @@ def roman_to_int(roman_string):
     if roman_string != str(roman_string) or roman_string is None:
         return 0
     symbols = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    nums = []
+    summation = 0
 
     i = 0
     while i < len(roman_string):
         if i + 1 < len(roman_string) and roman_string[i] + roman_string[i + 1]\
                 == "IX":
-            nums.append(9)
+            summation += 9
             i += 2
             continue
-        nums.append(int(symbols[roman_string[i].upper()]))
+        summation += symbols[roman_string[i].upper()]
         i += 1
-    return sum(nums)
+    return summation
