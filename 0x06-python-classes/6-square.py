1f8e74193d9e036f24e1bfca3549fa4f81f4bc80
Noah Chernet <noahchernet@yahoo.com>
2021-12-07 22:31:18 +0300

    Updated 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,7 +49,7 @@ class Square:
 
         """
         print() if self.size == 0 else None
-        print() if self.position[1] else None
+        print('\n' * self.position[1], end="")
         for x in range(self.size):
             for y in range(self.position[0]):
                 print(' ', end="")
