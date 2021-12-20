8dfe3a350f7a200568dfb46a726c22394faef823
Noah Chernet <noahchernet@yahoo.com>
2021-12-20 23:32:19 +0300

    Fixed ugly bug in task 7, 7-base_geometry.py. Added doctest for 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -21,6 +21,6 @@ class BaseGeometry:
         in particular
         """
         if type(value) is not int:
-            raise TypeError(name.join(" must be an integer"))
+            raise TypeError(name + " must be an integer")
         if value <= 0:
-            raise ValueError(name.join(" must be greater than 0"))
+            raise ValueError(name + " must be greater than 0")
