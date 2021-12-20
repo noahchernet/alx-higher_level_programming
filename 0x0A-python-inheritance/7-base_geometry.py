63dcabbce2d7130da8d8fc22aba50192240abc5b
Noah Chernet <noahchernet@yahoo.com>
2021-12-20 23:56:09 +0300

    Updated task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -20,7 +20,7 @@ class BaseGeometry:
         Raises appropriate exceptions with messages. Doesn't do anything
         in particular
         """
-        if type(value) is int and value <= 0:
+        if type(value) is int or type(value) is float and value <= 0:
             raise ValueError(name + " must be greater than 0")
         if type(value) != int:
             raise TypeError(name + " must be an integer")
