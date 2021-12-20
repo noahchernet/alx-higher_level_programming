dbd71a757c32ed73ebf9f1d1a3315f4ab6d99e08
Noah Chernet <noahchernet@yahoo.com>
2021-12-20 23:06:25 +0300

    Done task 6, 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""6-base_geometry
+Contains class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class with just one method
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
