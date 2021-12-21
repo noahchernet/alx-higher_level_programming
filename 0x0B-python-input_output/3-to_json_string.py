11fa022beb6a7c7f9bc33770176d8f831eac578c
Noah Chernet <noahchernet@yahoo.com>
2021-12-21 15:28:46 +0300

    Added docs

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 
 """3-to_json_string
 Contains function to_json_string
@@ -6,4 +7,13 @@ Contains function to_json_string
 
 
 def to_json_string(my_obj):
-    return __import__('json').dumps(my_obj)
+    """
+    Serializes a Python object
+
+    Args:
+        my_obj (object): a Python object
+
+    Return:
+        JSON object made from my_obj
+    """
+    return json.dumps(my_obj)
