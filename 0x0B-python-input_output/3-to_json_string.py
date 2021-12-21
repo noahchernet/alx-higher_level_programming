6c427acc4ad697f17cba6ce236c9f92d739b21a0
Noah Chernet <noahchernet@yahoo.com>
2021-12-21 15:27:14 +0300

    Updated 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import json
 
 """3-to_json_string
 Contains function to_json_string
@@ -7,4 +6,4 @@ Contains function to_json_string
 
 
 def to_json_string(my_obj):
-    return json.dumps(my_obj)
+    return __import__('json').dumps(my_obj)
