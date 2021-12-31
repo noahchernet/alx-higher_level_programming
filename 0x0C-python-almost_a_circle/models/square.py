dbe04c8788dd4cdd08a8d31e85a6d332733c1483
Noah Chernet <noahchernet@yahoo.com>
2021-12-31 20:51:16 +0300

    Done task 12, overridden update method to set size, and added a corresponding test function in test_square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,6 +32,34 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        """Updates some or all of the attributes based on how many arguments
+        are passed
+        If args' list index gets out of range i.e. it gets to the end of the
+        list, the updating of the attributes gets done
+
+        If args is empty, the values in kwargs are used to update the
+        attributes' values"""
+        if args:
+            try:
+                self.id = args[0]
+                self.width = args[1]
+                self.height = args[1]
+                self.x = args[2]
+                self.y = args[3]
+            except IndexError:
+                pass
+        else:
+            for key in kwargs.keys():
+                try:
+                    if key == "size":
+                        self.__setattr__("width", kwargs[key])
+                        self.__setattr__("height", kwargs[key])
+                        continue
+                    self.__setattr__(key, kwargs[key])
+                except AttributeError:
+                    pass
+
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
                                                  self.y, self.width)
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -17,8 +17,9 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(s.width, 4)
         self.assertEqual(s.height, 4)
         self.assertEqual(s.area(), 16)
-        s.update(4, 8, 8, 1, 2)
+        s.update(4, 8, 1, 2)
         self.assertEqual("[Square] (4) 1/2 - 8", s.__str__())
+        s.__del__()
 
     def test_invalid_setting_size(self):
 
@@ -35,6 +36,29 @@ class TestSquare(unittest.TestCase):
         with self.assertRaises(ValueError) as context:
             s.size = -1
         self.assertTrue("width must be > 0" in str(context.exception))
+
+    def test_update(self):
+        """Tests the overridden update method"""
+        s = Square(8)
+        s.update(12)
+        self.assertEqual("[Square] (12) 0/0 - 8", s.__str__())
+        s.update(7, 18, 2, 4, 5, 6, 7, 8, 9)
+        self.assertEqual("[Square] (7) 2/4 - 18", s.__str__())
+        s.update(7, 8)
+        self.assertEqual("[Square] (7) 2/4 - 8", s.__str__())
+        s.update(7, 8, 3)
+        self.assertEqual("[Square] (7) 3/4 - 8", s.__str__())
+
+        s.update(id=10)
+        self.assertEqual("[Square] (10) 3/4 - 8", s.__str__())
+        s.update(x=10)
+        self.assertEqual("[Square] (10) 10/4 - 8", s.__str__())
+        s.update(y=10)
+        self.assertEqual("[Square] (10) 10/10 - 8", s.__str__())
+        s.update(mnm=19)
+        self.assertEqual("[Square] (10) 10/10 - 8", s.__str__())
+        s.update(size_widht=8)
+        self.assertEqual("[Square] (10) 10/10 - 8", s.__str__())
 
 
 if __name__ == '__main__':
