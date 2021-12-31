a92c2a625ecb0a1ab72f244b7bb87212af93058d
Noah Chernet <noahchernet@yahoo.com>
2021-12-31 20:50:16 +0300

    rectangle.py - update uses setter rather than directly accessing the private variable

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,10 +77,10 @@ class Rectangle(Base):
         if args:
             try:
                 self.id = args[0]
-                self.__width = args[1]
-                self.__height = args[2]
-                self.__x = args[3]
-                self.__y = args[4]
+                self.width = args[1]
+                self.height = args[2]
+                self.x = args[3]
+                self.y = args[4]
             except IndexError:
                 pass
         else:
