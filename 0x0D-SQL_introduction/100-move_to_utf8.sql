58f1492df5508ab0e8122b8ef0ada91eb9af2c12
Noah Chernet <noahchernet@yahoo.com>
2022-02-02 00:40:47 +0300

    Updated answer for advanced task 1

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,4 +2,5 @@
 -- in your MySQL server.
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+UPDATE first_table SET name=CONVERT(name USING CHARACTER SET utf8mb4 COLLATE
+                                                             utf8mb4_unicode_ci);
