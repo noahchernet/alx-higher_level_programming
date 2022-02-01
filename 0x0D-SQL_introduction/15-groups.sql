961431a49cbe10a8e2376c8df69a4b20397f8caf
Noah Chernet <noahchernet@yahoo.com>
2022-02-02 00:16:49 +0300

    Done task 16, retrying task 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- lists the number of records with the same score in the table second_table
 -- of the database hbtn_0c_0 in your MySQL server.
 SELECT score, COUNT(score) AS number FROM second_table GROUP BY score HAVING
-COUNT(score) > 0;
+COUNT(score) > 1;
