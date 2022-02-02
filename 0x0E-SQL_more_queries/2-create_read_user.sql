5b6f872d1ea71d977127dce6cb243208697e2025
Noah Chernet <noahchernet@yahoo.com>
2022-02-02 22:14:15 +0300

    Updated task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- creates a user and grants specific permission to them
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
