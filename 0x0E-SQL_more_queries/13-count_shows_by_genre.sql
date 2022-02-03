457d52e225a58211021f904cb58073c6940bcf68
Noah Chernet <noahchernet@yahoo.com>
2022-02-03 12:19:42 +0300

    task 13 won't show NULL genre ids

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,4 +2,4 @@
 -- the number of shows linked to each.
 SELECT name AS genre, COUNT(*) AS number_of_shows FROM tv_genres LEFT JOIN
     tv_show_genres tsg ON tv_genres.id = tsg.genre_id
-    GROUP BY name ORDER BY number_of_shows DESC ;
+    WHERE tsg.genre_id IS NOT NULL GROUP BY name ORDER BY number_of_shows DESC;
