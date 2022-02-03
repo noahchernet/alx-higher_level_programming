ae873449eb8c0302b85f8377561efd2082d9a81d
Noah Chernet <noahchernet@yahoo.com>
2022-02-03 10:58:43 +0300

    Redone task 9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- lists all cities contained in the database hbtn_0d_usa. Lists the cities
+-- along with their corresponding state
+USE hbtn_0d_usa;
+SELECT table_1.id, table_1.name AS name, table_2.name AS name FROM cities
+    table_1, states table_2 WHERE table_1.state_id =table_2.id ORDER BY
+                                                                    table_1.id;
