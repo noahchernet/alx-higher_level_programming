bc157e4f8aa00110212d376984775c31a63c7535
Noah Chernet <noahchernet@yahoo.com>
2022-02-03 12:11:15 +0300

    SQL commands capitalized for task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- ists all genres from hbtn_0d_tvshows and displays
 -- the number of shows linked to each.
-SELECT name AS genre, COUNT(*) AS number_of_shows from tv_genres LEFT JOIN
-    tv_show_genres tsg on tv_genres.id = tsg.genre_id
+SELECT name AS genre, COUNT(*) AS number_of_shows FROM tv_genres LEFT JOIN
+    tv_show_genres tsg ON tv_genres.id = tsg.genre_id
     GROUP BY name ORDER BY number_of_shows DESC ;
