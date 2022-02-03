f4194020074d92d0cb57319173251a9445d24115
Noah Chernet <noahchernet@yahoo.com>
2022-02-03 10:17:02 +0300

    Redone task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,6 @@
 -- creates a user and grants specific permission to them
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+REVOKE ALL PRIVILEGES ON *.* FROM 'user_0d_2'@'localhost';
 GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
