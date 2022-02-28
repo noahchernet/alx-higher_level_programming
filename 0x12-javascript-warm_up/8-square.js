1d9e4c3af4468d0faacaba57831e8ec790360642
Noah Chernet <noahchernet@yahoo.com>
2022-02-28 13:48:24 +0300

    Done and added file for task 8, prints a square where its size is the argument passed

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const argv = require('process').argv;
+
+const squareSize = parseInt(argv[2]);
+
+if (isNaN(squareSize)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < squareSize; i++) {
+    let square = '';
+    for (let j = 0; j < squareSize; j++) {
+      square += 'X';
+    }
+    console.log(square);
+  }
+}
