08ce98499b2d91969e76212db56ed9c305d7c755
Noah Chernet <noahchernet@yahoo.com>
2022-03-01 19:34:11 +0300

    Done task 10, 10-converter.js returns a function that converts an number to a specified base. Run 10-main.js to see the results

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+exports.converter = function (base) {
+  return function (number) {
+    const newNumberInBase = [];
+    while (number) {
+      newNumberInBase.unshift(number % base);
+      number = Math.floor(number / base);
+    }
+
+    let strOfNewNumber = '';
+    newNumberInBase.forEach(function (item) { strOfNewNumber += item; });
+
+    // If the base is 16 and the number is between 10 and 15, assign it to the respective base 16 letter
+    strOfNewNumber = parseInt(strOfNewNumber);
+    if (base === 16) {
+      if (strOfNewNumber === 10) {
+        strOfNewNumber = 'a';
+      } else if (strOfNewNumber === 11) {
+        strOfNewNumber = 'b';
+      } else if (strOfNewNumber === 12) {
+        strOfNewNumber = 'c';
+      } else if (strOfNewNumber === 13) {
+        strOfNewNumber = 'd';
+      } else if (strOfNewNumber === 14) {
+        strOfNewNumber = 'e';
+      } else if (strOfNewNumber === 15) {
+        strOfNewNumber = 'f';
+      }
+    }
+
+    return strOfNewNumber;
+  };
+};
