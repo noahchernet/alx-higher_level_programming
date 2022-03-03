e78e8265034fb21143b513364fc332579540344a
Noah Chernet <noahchernet@yahoo.com>
2022-03-03 14:41:37 +0300

    Ordering by id for returned elements

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -16,7 +16,7 @@ def main():
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM states")
+    c.execute("SELECT * FROM states ORDER BY id")
     for i in c.fetchall():
         print(i)
 
