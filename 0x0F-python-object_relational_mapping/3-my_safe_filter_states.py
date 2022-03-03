8739e2ba4267f2ef4775684ec50ea743bc6de763
Noah Chernet <noahchernet@yahoo.com>
2022-03-03 16:59:06 +0300

    Testing task 3, 3-my_safe_filter_states.py if it correctly blocks SQL injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+"""
+Module 3-my_safe_filter_states
+Takes 4 arguments: username, password and database name to connect to the mysql
+serve running on localhost port 3306, and the name of
+the state being looked for.
+"""
+
+from sys import argv
+import MySQLdb
+
+
+def main():
+    """
+    Program starts here. Lists states with matching name passed as
+    the 4th argument from the database hbtn_0e_0_usa
+    """
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    c = db.cursor()
+
+    #  Clean SQL injection risky characters from the state name argument
+    state_name = argv[4]
+    escape_strings = ["\x00", "\n", "\r", "\\", "'", '\"', r"\xla"]
+    for i in escape_strings:
+        state_name = state_name.replace(i, "")
+
+    c.execute(
+        "SELECT * FROM states WHERE name='{}' ORDER BY id".format(argv[4]))
+    for i in c.fetchall():
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
