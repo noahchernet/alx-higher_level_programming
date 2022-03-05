5b58f5ffff5aee7650b43d8e0a6c3b39b6cad151
Noah Chernet <noahchernet@yahoo.com>
2022-03-05 19:33:04 +0300

    Done task 11, 11-model_state_insert.py, adds a State with name "Louisiana" into the states table

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""Module 11-model_state_insert
+Adds the State object “Louisiana” to the database hbtn_0e_6_usa
+(database name is passed as argument)"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def main():
+    """Program starts here.
+    Using SQLAlchemy, the 'states' table is loaded. A new State object
+    is created with name "Louisiana" and it's added to the states table."""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.add(State(name="Louisiana"))
+    session.commit()
+    print(session.query(State).filter(State.name == "Louisiana").all()[-1].id)
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
