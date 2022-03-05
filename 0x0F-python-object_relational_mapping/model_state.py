8a4c03b451c28cd30bff2c1d533708d7a5b2f996
Noah Chernet <noahchernet@yahoo.com>
2022-03-05 15:57:50 +0300

    Done task 6, model_state.py defines a State object which will be mapped to a database using SQLAlchemy.

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
