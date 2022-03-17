71a7b3fd5fe3a5888c5d74ca3a4a756302da3381
Noah Chernet <noahchernet@yahoo.com>
2022-03-17 18:20:25 +0300

    Done task 5, 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+# Takes in a URL, sends a POST request to the passed URL, and displays the body
+# of the response
+curl -X POST -d 'email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD' "$1"
