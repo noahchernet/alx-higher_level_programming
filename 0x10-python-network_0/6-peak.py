a362e5c0c22c8c84f50ce7ecfb75b16024ba8d35
Noah Chernet <noahchernet@yahoo.com>
2022-03-17 18:54:01 +0300

    Trying task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+'''Module 6-peak
+Contains function find_peak
+'''
+
+
+from re import L
+
+
+def find_peak(list_of_integers):
+    '''Finds the biggest number in list_of_integers
+    and returns it. If the list is empty, None is returned.
+    '''
+    if len(list_of_integers) == 0:
+        return None
+    return sorted(list_of_integers)[-1]
