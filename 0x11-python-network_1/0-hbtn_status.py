b08e161f85c9649272c7e8fd56e8829f2afe902e
Noah Chernet <noahchernet@yahoo.com>
2022-03-19 20:12:05 +0300

    Done task #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+'''
+Module 0-hbtn_status
+Fetches https://alx-intranet.hbtn.io/status
+'''
+import urllib.request
+
+def main():
+    '''Program starts here.
+    Details about the http response are printed.'''
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        print("Body response:")
+        print("\t- type:", type(response.read))
+        print("\t- content:", response.read())
+        print("\t- utf8 content:", response.msg)
+
+
+if __name__ == '__main__':
+    main()
