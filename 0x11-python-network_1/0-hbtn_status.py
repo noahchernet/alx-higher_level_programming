573522170888bb3f74578a741e7e6b909304f559
Noah Chernet <noahchernet@yahoo.com>
2022-03-19 20:19:44 +0300

    PEP8 and answer corrected, for task #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,13 +5,17 @@ Fetches https://alx-intranet.hbtn.io/status
 '''
 import urllib.request
 
+
 def main():
     '''Program starts here.
-    Details about the http response are printed.'''
+    Details about the http response
+    are printed.
+    '''
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        response_read = response.read()
         print("Body response:")
-        print("\t- type:", type(response.read))
-        print("\t- content:", response.read())
+        print("\t- type:", type(response_read))
+        print("\t- content:", response_read)
         print("\t- utf8 content:", response.msg)
 
 
