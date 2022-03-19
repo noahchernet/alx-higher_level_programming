b3f096bff23b16cebe5c20ca4a049a0ee8baccd1
Noah Chernet <noahchernet@yahoo.com>
2022-03-19 20:31:42 +0300

    Done task #1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+'''Module 1-hbtn_header
+Requests a URL and displays value of X-request-Id'''
+import urllib
+from sys import argv
+
+
+def main():
+    '''Program starts here. takes in a URL, sends a request to the URL and
+    displays the value of the X-Request-Id variable found in the
+    header of the response.'''
+    url = argv[1]
+    value = 'X-Request-Id'
+
+    with urllib.request.urlopen(url) as response:
+        print(response.headers.get('X-Request-Id'))
+
+
+if __name__ == '__main__':
+    main()
