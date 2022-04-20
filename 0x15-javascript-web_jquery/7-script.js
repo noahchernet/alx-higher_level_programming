07cc383deee7ec24f897f81080dfcb9de37d77ad
Noah Chernet <noahchernet@yahoo.com>
2022-04-20 20:32:46 +0300

    Done task 7

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $.ajax({
+    type: 'GET',
+    url: 'https://swapi-api.hbtn.io/api/people/5/?format=json',
+    success: function (data) {
+      $('#character').text(data.name);
+    }
+
+  });
+});
